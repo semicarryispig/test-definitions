348b12794e76b6f3ad7ce29a9b98905171aa3644
b51699 <b51699@freescale.com>
2014-12-16 14:27:47 +0800

    modify

--- scripts/suspend_random_auto.sh
+++ scripts/suspend_random_auto.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 i=0;
 t=2;
-d=5;
+d=2;
 r=0;
-while [ "$i" -lt 20000 ];
+while [ "$i" -lt 5000 ];
 do
-	/unit_tests/rtcwakeup.out -d rtc0 -m mem -s $t;
+	if [ "$t" -lt 5 ]; then
+		./scripts/rtcwakeup.out -d rtc0 -m standby -s $t;
+	else
+		./scripts/rtcwakeup.out -d rtc0 -m mem -s $t;
+	fi
+
 	sleep $d;
 	echo =============================
 	echo    suspend $i times;
 	echo =============================
 	i=`expr $i + 1`;
 	r=`date +%s`;
-	t=`expr $r % 10 + 2`;
-	d=`expr $r % 20 + 1`;
+	t=`expr $r % 6 + 3`;
+	d=`expr $r % 2 + 1`;
 	echo wakeup $t seconds, sleep $d seconds;
 done
